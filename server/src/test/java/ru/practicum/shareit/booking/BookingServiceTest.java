6e26b6936c640f8ba32e9cbef5458fdd034095ef
xawk <xawk0469@yandex.ru>
2025-03-26 15:21:29 +0300

    fix: checkstyle

--- server/src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
@@ -27,6 +28,7 @@ import java.util.List;
 
 @Slf4j
 @SpringBootTest
+@Transactional
 public class BookingServiceTest {
 
     @Autowired
